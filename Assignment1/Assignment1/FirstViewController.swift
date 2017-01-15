6537f6b3f60f45caf701998ad4a30ac4887d6940
Van Simmons <rvsrvs@gmail.com>
2017-01-15 13:37:43 -0500

    Fixing Assignment 1 to be a Tabbed View Controller

--- Assignment1/Assignment1/FirstViewController.swift
+++ Assignment1/Assignment1/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  Assignment1
 //
 //  Created by Van Simmons on 1/15/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
