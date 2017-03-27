03b0dede50e6a696118fe8faf73a5ce16bf660ea
yashspatel23 <yash.spatel23@gmail.com>
2017-03-27 13:37:32 -0700

    Problem 4: Draw Grid

--- Assignment3/Assignment3/GridView.swift
+++ Assignment3/Assignment3/GridView.swift
@@ -28,8 +28,49 @@ import UIKit
     var grid = Grid(20, 20)
     
     override func draw(_ rect: CGRect) {
+
+        let gridSize = CGSize(
+            width: rect.size.width / CGFloat(size),
+            height: rect.size.height / CGFloat(size)
+        )
+        
+        // Draw Grid
+        (0 ... size).forEach {
+            // Horizontal Lines
+            drawLine(
+                start: CGPoint(
+                    x: rect.origin.x,
+                    y: rect.origin.y + (gridSize.height * CGFloat($0))
+                ),
+                end: CGPoint(
+                    x: rect.origin.x + rect.size.width,
+                    y: rect.origin.y + (gridSize.height * CGFloat($0))
+                )
+            )
+            
+            // Vertical Lines
+            drawLine(
+                start: CGPoint(
+                    x: rect.origin.x + (gridSize.width * CGFloat($0)),
+                    y: rect.origin.y
+                ),
+                end: CGPoint(
+                    x: rect.origin.x + (gridSize.width * CGFloat($0)),
+                    y: rect.origin.y + rect.size.height
+                )
+            )
+        }
         
     }
     
-
+    
+    func drawLine(start: CGPoint, end: CGPoint){
+        let path = UIBezierPath()
+        path.lineWidth = gridWidth
+        path.move(to: start)
+        path.addLine(to: end)
+        gridColor.setStroke()
+        path.stroke()
+        
+    }
 }
