41a30e708843dd6b0ed320cd456aff9018ab76df
yashspatel23 <yash.spatel23@gmail.com>
2017-03-27 12:41:18 -0700

    Add GridView

---
+++ Assignment3/Assignment3/GridView.swift
@@ -0,0 +1,21 @@
+//
+//  GridView.swift
+//  Assignment3
+//
+//  Created by Yash Patel on 3/27/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class GridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
