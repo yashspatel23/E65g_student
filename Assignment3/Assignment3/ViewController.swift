6b3dd6aa25e1d058607d7ee6b6b0ef412e452a63
yashspatel23 <yash.spatel23@gmail.com>
2017-03-27 14:53:18 -0700

    Problem 6: Add Step Action and Outlet Handling

--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var StepBtn: UIButton!
+    @IBOutlet weak var GridView: GridView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stepBtnPressed(_ sender: Any) {
+        GridView.grid = GridView.grid.next()
+        GridView.setNeedsDisplay()
+    }
 
 }
 
